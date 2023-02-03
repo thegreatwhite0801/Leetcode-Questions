cb12a28ddb4d20842ea3db6566a3d3236ff29453
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-02-03 22:02:44 +0530

    Time: 553 ms (26.07%), Space: 10.3 MB (73.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,56 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> temp ; 
-    temp = nums ; 
-    int temp1 ,temp2 ; 
-    vector<int> tempest= {0,1} ; 
-   
-    vector<int> final ; 
-    bool flag1=true ,flag2 =true ; 
-        
-    sort(nums.begin() ,nums.end());
-    int frontpointer = 0 ,backpointer =nums.size()-1 ; 
-    while(frontpointer<backpointer){
-        if(nums[frontpointer]+nums[backpointer]<target){
-            
-            frontpointer+=1 ; 
+        vector<int>ans; 
+        for(int i= 0 ;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){ans.push_back(i);ans.push_back(j);return ans; }
+            }
         }
-        else if(nums[frontpointer]+nums[backpointer]>target){
-            backpointer-=1 ; 
-            
-        }
-        else {
-           
-               temp1 = nums[frontpointer] ; 
-               temp2 = nums[backpointer] ; 
-               frontpointer += backpointer ; 
-               
-            
-      }
-        
-        
-        
-        }
-        
-        
     
-    for(int i = 0 ; i <temp.size();i++){
-        if(temp1==temp[i]&&flag1==true){
-            final.push_back(i) ; 
-            flag1==false ; 
-        }
-        else if(temp2==temp[i]&&flag2==true){
-            final.push_back(i);
-        flag2=false ; 
-        
-        }
-        
-    }     
-    
-    
-    return final ; 
-    
-    
+    return ans ; 
     }
 };
