1ddc1c3203cb5ddc4e76536cbee73feeae333de2
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-02-03 22:34:53 +0530

    Time: 12 ms (85.14%), Space: 10.7 MB (46.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans; 
+  /*      vector<int>ans; 
         for(int i= 0 ;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){ans.push_back(i);ans.push_back(j);return ans; }
@@ -9,5 +9,23 @@ public:
         }
     
     return ans ; 
+    } */
+        unordered_map<int,int>map1; 
+        vector<int>ans;
+        for(int i = 0 ;i<nums.size();i++){
+            int temp =target-nums[i]; 
+            
+                if(map1.find(temp)!=map1.end()){
+                    ans.push_back(map1[temp]); 
+                    ans.push_back(i); 
+                return ans; 
+                }
+            
+        map1[nums[i]] = i ; 
+        
+        
+        }
+    return ans ;    
     }
+        
 };
