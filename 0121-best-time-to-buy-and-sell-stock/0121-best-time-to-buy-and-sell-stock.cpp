0192eb7fb8119ceaffafa3027e1f6cc75da4cfeb
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-02-13 21:35:41 +0530

    Time: 135 ms (77.96%), Space: 93.4 MB (56.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-   int minu = INT_MAX ; 
-  int maxu = 0 ; 
-int pointer = 0 ; 
-    while(pointer<prices.size()){ 
-    minu = min(prices[pointer],minu); 
-    maxu =max(maxu,prices[pointer]-minu) ; 
-    pointer+=1 ; 
-    }
+        int minm= INT_MAX ; 
+        int maxm =0 ;
+        
+        for(auto x :prices){
+            minm =min(x,minm) ;
+            maxm= max(maxm,x-minm); 
+            
+        }
     
-    return maxu ; 
+    return maxm ; 
     }
-} ;
+};
