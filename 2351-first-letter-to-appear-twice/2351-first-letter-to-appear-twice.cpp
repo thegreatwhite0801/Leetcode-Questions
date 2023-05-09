44e671149797f81e4de7109d4002ee98faa06339
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-10 00:18:13 +0400

    Time: 4 ms (31.46%), Space: 6.1 MB (58.67%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int>map1; 
+        for(auto x :s){
+            map1[x]++ ;
+            if(map1[x]==2)return x; 
+        }
+    return 'a'; 
+    }
+};
