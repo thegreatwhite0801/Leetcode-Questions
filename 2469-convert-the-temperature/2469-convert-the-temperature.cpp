637d447efa8366a62b5edaff02e22317815b3eeb
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-09 23:12:58 +0400

    Time: 0 ms (100.00%), Space: 6.2 MB (5.38%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-       vector<double>temp ; 
-        double kelvin = celsius+ 273.15; 
-        double fanheit = celsius*1.80 + 32 ; 
-        temp.push_back(kelvin); 
-        temp.push_back(fanheit); 
-    return temp ; 
+    vector<double>temp ; 
+        temp.push_back(celsius+273.15);
+        temp.push_back(celsius*1.80+32); 
+        return temp; 
     }
 };
