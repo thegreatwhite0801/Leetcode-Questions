585edfefe6e6513a838d0e1ca3715b3e47ebad66
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-25 20:55:04 +0400

    Time: 40 ms (59.16%), Space: 20.6 MB (63.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>map1; 
-        for(auto x :strs){
-          string t = x; 
-          sort(t.begin(),t.end()); 
-          map1[t].push_back(x) ;  
-            }
+     unordered_map<string,vector<string>>map1 ; 
+    for(auto x : strs){ 
+    string temp = x ;
+      sort(temp.begin(),temp.end()); 
+      map1[temp].push_back(x); 
     
-    vector<vector<string>>ans; 
-        for(auto x: map1){
-            ans.push_back(x.second); 
+    
+    }
+    
+    vector<vector<string>>final_ans; 
+        for(auto it :map1){
+            final_ans.push_back(it.second); 
         }
-    return ans ;
-    
+    return final_ans ; 
     }
 };
