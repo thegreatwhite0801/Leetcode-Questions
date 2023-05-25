bdff5b1cc22b537ffbd541002f63463e922cb4d5
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-25 22:48:08 +0400

    Time: 11 ms (96.76%), Space: 13.5 MB (85.30%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>map1; 
-        for(auto x : nums){
-            map1[x]++ ;
+     unordered_map<int,int> map1 ;
+        for(auto x :nums){
+            map1[x]++;
         }
-    vector<int>ans; 
- priority_queue<pair<int,int>>q1; 
-        for(auto it:map1){
-            
-            q1.push(make_pair(it.second,it.first)); 
-            }
+    
+    priority_queue<pair<int,int> >pq; 
+        
+        for(auto it : map1){ 
+    pq.push(make_pair(it.second,it.first)); 
     
     
-    while(k--){
-        ans.push_back(q1.top().second); 
-        q1.pop();
     }
-    return ans; 
+  vector<int>ans; 
+        while(k--){
+        pair<int,int>top1 = pq.top(); 
+        ans.push_back(top1.second); 
+            pq.pop(); 
+    }
+    return ans ;
+    
+    
+    
     }
 };
