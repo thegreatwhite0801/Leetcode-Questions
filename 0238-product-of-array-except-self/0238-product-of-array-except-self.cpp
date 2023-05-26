d48a996a7ce16f7f477ad753faae657133c35f23
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-26 16:47:33 +0400

    Time: 29 ms (50.88%), Space: 24.1 MB (53.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>ans ; 
-        int cunt = count(nums.begin(),nums.end(),0); 
-        if(cunt>1)return vector<int>(nums.size(),0); 
-        if(cunt==0){
-            int product =1 ;
-            for(auto x : nums){
-                product*=x ;
-            }
+        vector<int>ans = nums ; 
+        int product = 1 ; 
+        bool flag= false; 
+      int count = 0 ; 
+        for(auto x :nums){
+           if(x!=0){
+               product*=x ; 
+               
+           }
+        else{
+            count+=1; 
             
-            for(auto x:nums ){
-             ans.push_back(product/x);    
-            }
-                
         }
-    if(cunt==1){
-        int product=1 ; 
-        for(auto x : nums){
-         if(x!=0)product*=x; 
-        }
-       for(auto x:nums){
-           if(x==0)ans.push_back(product);
-           else ans.push_back(0); 
        }
-    
-    
+    if(count>1){
+        vector<int>final2(nums.size(),0); 
+        return final2; 
     }
-    
-    
+    else if(count==0){
+      for(int i = 0 ; i<ans.size();i++){
+          ans[i]=product/ans[i]; 
+      } 
+    }
+    else { 
+        for(int i = 0 ; i<ans.size();i++){
+        if(ans[i]!=0){
+            ans[i]=0; 
+        }
+        else{ 
+        ans[i]=product; 
+        }
+        } 
+        }
     return ans ; 
+    
     }
 };
