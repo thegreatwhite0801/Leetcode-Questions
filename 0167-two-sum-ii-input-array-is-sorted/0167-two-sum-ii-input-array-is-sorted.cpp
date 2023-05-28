21c77fa606dbf160728f4b0ea7c86f591a6ad9e9
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-05-28 22:37:26 +0400

    Time: 12 ms (74.82%), Space: 15.6 MB (48.44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int,int>map1; 
-        for(int i = 0 ;i<numbers.size();i++){
-            map1[numbers[i]]=i; 
-        }
+        
+       int front = 0 ; 
+        int back = numbers.size()-1; 
         vector<int>ans; 
-        
-        for(int i= 0 ;i<=numbers.size();i++){
-         if(map1.find(target-numbers[i])!=map1.end()){
-  ans.push_back(i+1);
-             ans.push_back(map1[target-numbers[i]]+1); 
-         return ans; 
-         }    
+        for(int i = 0 ; i<numbers.size();i++){
             
-        
-        
-        
-        
+            
+            if(numbers[front]+numbers[back]==target){
+                ans.push_back(front+1) ;
+                ans.push_back(back+1) ;
+                break ; 
+            } 
+            if(numbers[front]+numbers[back]>target){
+                 back-=1; 
+            }
+            if(numbers[front]+numbers[back]<target){
+                front+=1; 
+            }
+            
+            
         }
-        
-        
-     return ans ;   
-        
+    
+    
+    return ans ;
+    
+    
     }
 };
