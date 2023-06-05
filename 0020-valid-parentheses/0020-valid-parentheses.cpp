defc06a87c8fdb3750b410652f341eb7c7bc1d0f
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-06-05 21:06:44 +0400

    Time: 4 ms (39.34%), Space: 6.4 MB (10.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char>st;   
+      for(auto x :s){
+          if(x=='[' ||x=='('||x=='{'){
+              st.push(x);
+          }
+      else{
+           if(st.empty()||(x==')'&&st.top()!='(')||(x==']'&&st.top()!='[')||(x=='}'&&st.top()!='{')){
+               return false; 
+           }
+    st.pop(); 
+      }
+        
+      
+      }  
+    
+    return st.empty(); 
+    }
+};
