e095e3840f7b612aaf49c2dabdacbecc2eaf37f4
Cantsleep08 <108234210+Vishal0801@users.noreply.github.com>
2023-06-11 17:40:06 +0400

    Time: 0 ms (100.00%), Space: 8.2 MB (12.35%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>temp  = heights; 
+        sort(temp.begin(),temp.end());
+       int count = 0 ; 
+        for(int i = 0 ; i<heights.size();i++){
+           if(temp[i]!=heights[i])count++; 
+        }
+    return count ; 
+    }
+};
